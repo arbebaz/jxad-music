21fb164d45bcf6599a454d25adb0a7f4bb26585f
abeba damasse <abeba.dms@icloud.com>
2024-05-17 22:01:42 +0200

    backend

---
+++ backend/src/main/java/com/jxadmusic/jxadmusic/JxadMusicApplication.java
@@ -0,0 +1,13 @@
+package com.jxadmusic.jxadmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxadMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxadMusicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jxadmusic/jxadmusic/JxadMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxadmusic.jxadmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxadMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
