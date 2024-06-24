6aa7a361d8a7eb554f1a5dd51be3ab38aa434762
abeba damasse <abeba.dms@icloud.com>
2024-06-24 21:39:50 +0200

    Page Inscription

--- frontend/jxad-music/src/app/pages/general/register/register.component.ts
+++ frontend/jxad-music/src/app/pages/general/register/register.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../../components/header/header.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
