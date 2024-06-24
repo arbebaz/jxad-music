80be3b83dd7aa5ca02c2e433cfc8368a031e1ecd
abeba damasse <abeba.dms@icloud.com>
2024-06-24 20:52:56 +0200

    FOOTER

--- frontend/jxad-music/src/app/pages/general/home/home.component.ts
+++ frontend/jxad-music/src/app/pages/general/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../components/header/header.component';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterLink, CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
